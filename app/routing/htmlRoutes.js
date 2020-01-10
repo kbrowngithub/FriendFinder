bc45b3c1a07d0865c8f49b2481bf82b251bddb41
Kevin Brown <48394009+kbrowngithub@users.noreply.github.com>
2020-01-10 15:23:26 -0700

    Fixed bug in htmlRoutes.js for home page

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -19,8 +19,8 @@ module.exports = function(app) {
     res.sendFile(path.join(__dirname, "../public/survey.html"));
   });
 
-  app.get("/test", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/test.html"));
+  app.get("/home", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/home.html"));
   });
 
   // If no matching route is found default to home
